5935742be873d22614bb94190d2fa6bcee7710ca
Joan López Ferrer <trombonet12@gmail.com>
2023-10-23 13:39:51 +0200

    Host per node funcional

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // Import required modules
 const express = require('express');
 const mysql = require('mysql');
+const path = require('path');
 
 // Create an instance of express
 const app = express();
+
+// Middlewares
+app.use(express.json());
 
 // Define the database connection details
 const db = mysql.createConnection({
@@ -26,7 +30,4 @@ app.listen('3000', () => {
     console.log('Server started on port 3000');
 });
 
-// Route to serve inicio.html
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+app.use('/', express.static(path.join(__dirname, '')));
